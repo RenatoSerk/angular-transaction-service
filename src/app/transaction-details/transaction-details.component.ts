91ce114db1526a5a2ead04cd61108037b55f75b2
RenatoSerk <brickworked@gmail.com>
2022-08-14 22:17:21 -0400

    Added transaction details component
    - Implemented display of the transaction items's id, date and comments
    - Added the ability to edit the comment with a material input

--- src/app/transaction-details/transaction-details.component.ts
+++ src/app/transaction-details/transaction-details.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { Subject } from 'rxjs';
+import { Transaction } from '../models/transaction';
 
 @Component({
   selector: 'app-transaction-details',
   templateUrl: './transaction-details.component.html',
   styleUrls: ['./transaction-details.component.scss']
 })
-export class TransactionDetailsComponent implements OnInit {
+export class TransactionDetailsComponent implements OnDestroy {
 
-  constructor() { }
+  _onDestroy = new Subject<void>();
+  transactionDetails: Transaction;
+  commentValue: string;
 
-  ngOnInit(): void {
+  constructor(@Inject(MAT_DIALOG_DATA) transactionDetails: Transaction, private dialogRef: MatDialogRef<TransactionDetailsComponent>) { 
+    this.transactionDetails = transactionDetails;
+    this.commentValue = this.transactionDetails.comments;
+  }
+
+  ngOnDestroy(): void {
+    this._onDestroy.next();
+    this._onDestroy.complete();
+  }
+
+  updateCommentAndClose(){
+    this.dialogRef.close(this.commentValue);
   }
 
 }
